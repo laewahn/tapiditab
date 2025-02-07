d5fcd60377781492e186e7f338f0586b6039fb24
Dennis Lewandowski <laewahn@me.com>
2025-02-07 22:40:59 +0100

    Project init

---
+++ TapidiTab/TapidiTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapidiTabApp.swift
+//  TapidiTab
+//
+//  Created by Dennis Lewandowski on 07.02.25.
+//
+
+import SwiftUI
+
+@main
+struct TapidiTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TapidiTabTests/TapidiTabTests.swift
@@ -0,0 +1,17 @@
+//
+//  TapidiTabTests.swift
+//  TapidiTabTests
+//
+//  Created by Dennis Lewandowski on 07.02.25.
+//
+
+import Testing
+@testable import TapidiTab
+
+struct TapidiTabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
